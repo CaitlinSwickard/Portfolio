80a35d6531501655f28a75db8e032c2950d23a45
Caitlin Swickard <caitlin6029@gmail.com>
2021-06-16 22:35:22 -0600

    Correcting a tag links and css updates

--- src/script.js
+++ src/script.js
@@ -1,2 +1,8 @@
 const burgerButton = document.getElementById('hamburger');
 const navList = document.getElementById('nav-list');
+
+function toggleButton() {
+  navList.classList.toggle('show');
+}
+
+burgerButton.addEventListener('click', toggleButton);
